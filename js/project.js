1dff7985a97c0078ad00f7a72fe8159e707c525e
Peter 4A5A <498883@edu.rocmn.nl>
2017-10-03 12:48:37 +0200

    Added version 2

--- js/project.js
+++ js/project.js
@@ -1,22 +1,31 @@
 /**
  * Selects all element that will it will find
  * @param  {[string]} elements [The selector for all the elements]
- * @return {[array]}          [All the elements that have been found]
+ * @return {[obj]}          Returns a function for the element as a object
  */
 function selectAll(elements) {
   let element = document.querySelectorAll(elements);
-  let saveFunction = function(attribuut, option) {
-    let savedElement = element;
+  let saveFunction = {
+    savedElement: element,
+    style: function(applyingStyle, option) {
+      let savedElement = this.savedElement;
+      for (let i = 0; i < savedElement.length; i++) {
+            savedElement[i].style[applyingStyle] = option;
+            // We use applyingStyle between [] we remove the dot 
+      }
+    },
 
-    for (let i = 0; i < savedElement.length; i++) {
-          savedElement[i].setAttribute(attribuut, option);
-    }
-
+    attribuut: function(attribuut, option) {
+      let savedElement = this.savedElement;
+      for (let i = 0; i < savedElement.length; i++) {
+            savedElement[i].setAttribute(attribuut, option);
+      }
+    },
   }
   return(saveFunction);
 }
 
 var test = selectAll(".test");
-test('width', '200px');
-test('height', '200px');
-test('background-color', 'red');
+test.attribuut('width', '200px');
+test.style('height', '200px');
+test.style('background-color', 'red');
